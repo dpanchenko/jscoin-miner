00f0d893a01f871ed071a488b9240351d66c89ef
Dmitry Panchenko <dmitriy.panchenko@gmail.com>
2018-06-08 17:52:22 +0300

    Fix type: condition in consensus algo

--- core/classes/chain.js
+++ core/classes/chain.js
@@ -85,7 +85,7 @@ class Chain {
         remoteLongestChain = chainData;
       }
     });
-    if (this.chain.length <= remoteLongestChain.blocks.length) {
+    if (this.chain.length > remoteLongestChain.blocks.length) {
       log('use local chain as consistent');
       return;
     }
